3bb4d82b91c44ef639c67e6843d58161d92f7985
Bodacious <gavin@gavinmorrice.com>
2018-03-07 21:59:20 +0000

    Add fix to ensure log dir exists

--- spec/support/api_requests.rb
+++ spec/support/api_requests.rb
@@ -6,7 +6,9 @@ RSpec.configure do |config|
     config.include StubApiRequestHelper
 
     Evvnt.configure do |config|
-      log_path = File.join(File.dirname(__FILE__), "..", "..", "log", "test.log")
+      dir_path = File.join(File.dirname(__FILE__), "..", "..", "log")
+      log_path = File.join(dir_path, "test.log")
+      FileUtils.mkdir(dir_path) unless File.exist?(dir_path)
       FileUtils.touch(log_path) unless File.exist?(log_path)
 
       config.logger     = Logger.new(log_path)
