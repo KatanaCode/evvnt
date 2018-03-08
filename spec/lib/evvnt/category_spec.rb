3979c7588a29a61fb099c9cdc9d54ba47f40ef97
Bodacious <gavin@gavinmorrice.com>
2018-03-08 16:23:25 +0000

    Add specs for categories API

--- spec/lib/evvnt/category_spec.rb
+++ spec/lib/evvnt/category_spec.rb
@@ -1,7 +1,33 @@
 require "spec_helper"
 
-describe Evvnt::Category do
+describe Evvnt::Category, type: :api do
 
-  pending
+  describe "::index" do
+
+    before do
+      stub_api_request(:get, "categories")
+    end
+
+    subject { Evvnt::Category.index }
+
+    it "returns an Array" do
+      expect(subject).to be_an(Array)
+    end
+
+    it "returns Categories" do
+      subject.each do |category|
+        expect(category).to be_a(Evvnt::Category)
+      end
+    end
+
+    it "has the expected attributes" do
+      subject.each do |category|
+        expect(category).to respond_to :id
+        expect(category).to respond_to :parent_id
+        expect(category).to respond_to :name
+      end
+    end
+
+  end
 
 end
